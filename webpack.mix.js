ed1757ca0d4b98f4b320df9574db9c346b7faec9
closemdev <development@closem.a>
2025-02-20 18:16:00 -0500

    Updated webpack.mix.js to ensure Vue builds correctly

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,10 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js('resources/js/app.js', 'public/dist/assets/js')
+   .vue()
+   .sass('resources/sass/app.scss', 'public/dist/assets/css')
+   .postCss('resources/css/app.css', 'public/dist/assets/css', [
+       require('tailwindcss'),
+   ])
+   .version();
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
